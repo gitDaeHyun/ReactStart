bf6cb2b0f882acd9af0b0bb548198518b40038ac
전대현 <wjseogus523@naver.com>
2023-07-07 15:50:53 +0900

    CSS Practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
+import menu1 from './routes/menu1';
+import menu2 from './routes/menu2';
+import menu3 from './routes/menu3';
+import menu4 from './routes/menu4';
+import Navigation from './components/navigation';
+
+
+
 
 function App() {
 	return (
 		<Router>
+			<Navigation />
 			<Switch>
-				<Route path='/movie/:id'>
-					<Detail />
-				</Route>
-				<Route path='/'>
-					<Home />
-				</Route>
+				<Route component={Detail} path='/movie/:id' />
+				<Route component={Home} path='/' />
+				<Route component={menu1} path='/menu1' />
+				<Route component={menu2} path='/menu2'/>
+				<Route component={menu3} path='/menu3'/>
+				<Route component={menu4} path='/menu4'/>
 			</Switch>
 		</Router>
 	);
